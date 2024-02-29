9b2c57d08cc2380b5832c7b2b4690cf66efdef4e
keval <kevalgajjar1311@gmail.com>
2024-02-29 23:29:05 +0530

    Update components

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,37 +5,41 @@ import Input from './Input';
 
 
 export default function Home() {
-    const [temp, setTemp] = useState([]);
-    const [id, setId] = useState(0);
+    const [todo, setTodo] = useState([]);
+    const [id, setId] = useState(1);
 
+    // To fecth previously stored todo
     useEffect(() => {
         const storedTodos = JSON.parse(localStorage.getItem("todo"));
-
-        if (storedTodos) {
-            setTemp(storedTodos);
+        if (storedTodos && storedTodos.length > 0) {
+            setTodo(storedTodos);
             const maxId = Math.max(...storedTodos.map(todo => todo.id), 0);
             setId(maxId + 1);
         }
     }, []);
 
+    // To update the data in localstorage 
+    useEffect(() => {
+        localStorage.setItem("todo", JSON.stringify(todo));
+    }, [todo])
+
+    // To add new todo
     const handleClick = (val) => {
-        const newValue = val;
-        if (newValue.trim() !== "") {
-            setTemp([...temp, { id: id, completed: "0", todo: newValue }]);
-            localStorage.setItem("todo", JSON.stringify([...temp, { id: id, completed: "0", todo: newValue }]));
-            setId(id + 1);
+        if (val.trim() !== "") {
+            setTodo([...todo, { id: id, completed: false, todo: val }]);
+            setId(oldid => oldid + 1);
         }
     }
 
     return (
         <div className='d-flex justify-content-center align-items-center p-3' style={{ width: "100vw", height: "100vh", background: "#141414" }}>
-            <div className="card" style={{ width: "25rem", height: "30rem" }}>
+            <div className="card" id='card' style={{ width: "38rem", height: "35rem" }}>
                 <div className="card-body" >
 
                     <Input handleClick={handleClick} />
 
                     <div className='d-flex flex-column align-items-center scrollable-area' style={{ height: "25rem", overflowY: "scroll", overflowX: "hidden" }} >
-                        <Items temp={temp} setTemp={setTemp} />
+                        <Items todo={todo} setTodo={setTodo} />
                     </div>
 
                 </div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 export default function Input(props) {
     const { handleClick } = props;
+    const [data, setData] = React.useState("");
 
     const handle = (e) => {
         e.preventDefault();
-        let input = document.getElementById("input");
-        const newValue = input.value;
-        handleClick(newValue);
-        input = "";
+        handleClick(data);
+        setData("");
     }
 
     return (
         <div id='inputd' className='mb-2'>
             <form action="#" id='form'>
-                <input style={{ width: "85%", height: "100%" }} className='ps-2' type="text" id='input' name='input' placeholder='Enter todo' />
-
+                <input className='ps-2' type="text" id='input'
+                    value={data} onChange={(e) => setData(e.target.value)}
+                    placeholder='Enter todo' />
                 <button type='submit' id='submit' className='text-light' onClick={handle} ><i className="fa-solid fa-plus"></i></button>
             </form>
         </div>
